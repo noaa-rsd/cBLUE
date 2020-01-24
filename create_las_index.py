89a386da202cd615a75bfe809808edfc65caed19
Nick Forfinski-Sarkozi <nick.forfinski@gmail.com>
2020-01-24 13:43:43 -0500

    updated las index code

--- create_las_index.py
+++ create_las_index.py
@@ -1,26 +1,14 @@
 from pathlib import Path
 from shapely.geometry import Polygon
-from shapely.ops import transform, unary_union
+from shapely.ops import transform
 import pyproj
 from pyproj import CRS
 from functools import partial
 import subprocess
 import json
-import pdal
 from osgeo import osr
 import geopandas as gpd
 from tqdm import tqdm
-
-
-dir1 = Path(r'\\ngs-s-rsd\Lidar_Proc01\2016\FL1604-TB-N-880_MarcoIsland_p\06_RIEGL_PROC\04_EXPORT\Green\04_FL1604-TB-N-880_g_gpsa_rf_ip_wsf_r_adj_qc')
-lasses = dir1.rglob('*.las')
-bboxes = []
-attributes = {
-    'path': [],
-    'hor_srs': [],
-    }
-
-wgs84_epsg = 'epsg:4326'
 
 
 def run_console_cmd(cmd):
@@ -33,8 +21,21 @@ def run_console_cmd(cmd):
     return returncode, output
 
 
-for las_path in tqdm(list(lasses)[0:20]):
-    print(las_path)
+bboxes = []
+attributes = {
+    'name': [],
+    'parent': [],
+    'hor_srs': [],
+    'las_version': [],
+    }
+
+wgs84_epsg = 'epsg:4326'
+gpkg = Path(r'z:\las_files.gpkg')
+
+las_dir = Path(r'T:\2017')
+lasses = list(las_dir.rglob('*.las'))
+
+for las_path in tqdm(lasses):
 
     las = str(las_path).replace('\\', '/')
     cmd_str = 'pdal info {} --metadata'.format(las)
@@ -43,8 +44,11 @@ for las_path in tqdm(list(lasses)[0:20]):
         metadata = run_console_cmd(cmd_str)[1].decode('utf-8')
         meta_dict = json.loads(metadata)['metadata']
 
-        srs = meta_dict['srs']
-        hor_wkt = srs['horizontal']
+        major_version = meta_dict['major_version']
+        minor_version = meta_dict['minor_version']
+        las_version = f'{major_version}.{minor_version}'
+
+        hor_wkt = meta_dict['srs']['horizontal']
         hor_srs = osr.SpatialReference(wkt=hor_wkt) 
         projcs = hor_srs.GetAttrValue('projcs')
 
@@ -65,24 +69,25 @@ for las_path in tqdm(list(lasses)[0:20]):
             pyproj.Proj(CRS.from_string(projcs)),
             pyproj.Proj(wgs84_epsg))
 
-        poly = transform(project, Polygon(tile_coords))
-        bboxes.append(poly)
+        bboxes.append(transform(project, Polygon(tile_coords)))
 
-        path_parts = las_path.parts
-
-        attributes['path'].append(str(las_path))
+        attributes['name'].append(las_path.name)
+        attributes['parent'].append(str(las_path.parent))
         attributes['hor_srs'].append(projcs)
+        attributes['las_version'].append(las_version)
 
     except Exception as e:
-        print(e)
+        print(f'{e} - {str(las_path)}')
 
-geom = [unary_union(bboxes)]
+print('creating GeoDataFrame...')
 gdf = gpd.GeoDataFrame(geometry=bboxes, crs=wgs84_epsg)
-print(gdf)
-gdf['path'] = attributes['path']
-gdf['hor_srs'] = attributes['hor_srs']
-print(gdf)
+gdf.geometry = gdf.geometry.map(lambda poly: transform(lambda x, y: (y, x), poly))
 
-gpkg = Path(r'z:\las_files.gpkg')
-layer = '_2016'
+gdf['name'] = attributes['name']
+gdf['parent'] = attributes['parent']
+gdf['hor_srs'] = attributes['hor_srs']
+gdf['las_version'] = attributes['las_version']
+
+print('writing geopackage...')
+layer = f'Lidar_Proc01__2017'
 gdf.to_file(gpkg, layer=layer, driver='GPKG')
